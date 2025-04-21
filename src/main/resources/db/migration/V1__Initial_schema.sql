b0067fd74fb7d81f324906eb8fc9e8124f822b3d
LepoldRB <zanozin.dan@yandex.ru>
2025-04-21 15:58:06 +0300

    flyway_migr

---
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -0,0 +1,27 @@
+-- Создание таблицы пассажиров
+CREATE TABLE t_passenger (
+    id BIGSERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    passport_number VARCHAR(255) NOT NULL UNIQUE,
+    contact_info VARCHAR(255)
+);
+
+-- Создание таблицы рейсов
+CREATE TABLE t_flight (
+    id BIGSERIAL PRIMARY KEY,
+    flight_number VARCHAR(255) NOT NULL,
+    departure VARCHAR(255) NOT NULL,
+    destination VARCHAR(255) NOT NULL,
+    departure_time TIMESTAMP NOT NULL,
+    capacity INTEGER NOT NULL
+);
+
+-- Создание таблицы бронирований
+CREATE TABLE t_booking (
+    id BIGSERIAL PRIMARY KEY,
+    booking_time TIMESTAMP NOT NULL,
+    seat_class VARCHAR(255) NOT NULL,
+    seat_number VARCHAR(255) NOT NULL,
+    flight_id BIGINT NOT NULL REFERENCES t_flight(id) ON DELETE CASCADE,
+    passenger_id BIGINT NOT NULL REFERENCES t_passenger(id) ON DELETE CASCADE
+);
